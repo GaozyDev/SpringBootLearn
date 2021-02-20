41d37a03c168feef73250421202e77d279e92102
GaozyDev <670453367@qq.com>
2021-02-20 18:37:31 +0800

    卖家端类目

---
+++ src/main/java/com/gl/springbootlearn/controller/SellerCategoryController.java
@@ -0,0 +1,39 @@
+package com.gl.springbootlearn.controller;
+
+import com.gl.springbootlearn.dataobject.ProductCategory;
+import com.gl.springbootlearn.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/seller/category")
+public class SellerCategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/list")
+    public ModelAndView list(Map<String, Object> map) {
+        List<ProductCategory> categoryList = categoryService.findAll();
+        map.put("categoryList", categoryList);
+        return new ModelAndView("category/list", map);
+    }
+
+    @GetMapping("/index")
+    public ModelAndView index(@RequestParam(value = "CategoryId", required = false) Integer categoryId,
+                              Map<String, Object> map) {
+        if (categoryId != null) {
+            ProductCategory productCategory = categoryService.findOne(categoryId);
+            map.put("category", productCategory);
+        }
+
+        return new ModelAndView("category/index", map);
+    }
+}
